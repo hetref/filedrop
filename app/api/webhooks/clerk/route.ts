cfb763a88c5cdd8a0724bbe94fcb9f08507444a7
Aryan shinde <shindearyan179@gmail.com>
2024-07-13 03:10:43 +0530

    Middleware Changes

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
-import { createUser } from "@/lib/actions/user";
+// import { createUser } from "@/lib/actions/user";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
